619a37c9b050c919047819f126f600a45621a32e
HarshGarg <157905522+harshgarg123456789@users.noreply.github.com>
2024-08-17 22:29:30 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ const port = 4000
 const cors = require('cors')
 const router = require('./Routes/router')
 
-app.use(cors());
+app.use(
+  cors({
+      origin:"*",
+      credentials:true,
+  })
+)
 app.use(express.json());
 app.use(router);
 
